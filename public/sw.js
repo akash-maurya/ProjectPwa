baca3cc55ee7559f4ea7d09c16f335b6830a5212
akash-maurya <akashmaurya2301@gmail.com>
2021-11-04 11:45:58 +0530

    improved caching

--- public/sw.js
+++ public/sw.js
@@ -120,6 +120,24 @@ define("./sw.js", ["./workbox-1ffba242"], function (workbox) {
 });
 //# sourceMappingURL=sw.js.map
 
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request).then(function (res) {
+          return caches.open("dynamic").then(function (cache) {
+            cache.put(event.request.url, res.clone());
+            return res;
+          });
+        });
+      }
+    })
+  );
+});
+
 self.addEventListener("sync", function (event) {
   console.log("  [Service Worker] Background Syncing  :)" + event);
 
