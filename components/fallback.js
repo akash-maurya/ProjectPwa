5e33721c0d619b686c497309517ffee3e51e2ce1
licious <licious@LANCELOT.local>
2021-11-19 16:23:55 +0530

    fallback

--- components/fallback.js
+++ components/fallback.js
@@ -18,11 +18,8 @@ const fallback  = ()=>{
           </div>
 
           <div className={style.content}>
-            <h1 className={style.h1}>Page Not Found!</h1>
+            <h1 className={style.h1}>No Connection</h1>
             <div>
-              <p className={style.para}>
-                We were unable to load the page you requested.
-              </p>
               <p className={style.para}>
                 Please check your internet connection and try again
               </p>
